c289c8b597356570f8397c55d96c2e9aabf39ed4
nigatubb32-cell <nigatubb32@gmail.com>
2026-08-14 23:15:11 +0300

    Initialize Flask app with static file serving

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def home():
+    return send_from_directory('static', 'index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
